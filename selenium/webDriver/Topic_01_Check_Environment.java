8fc472aeb1aae5ef03f274be84de5db779500b15
Giang My Hoa <hoafgm@gmail.com>
2024-06-22 10:53:58 +0700

    test chảng name

--- selenium/webDriver/Topic_01_Check_Environment.java
+++ selenium/webDriver/Topic_01_Check_Environment.java
@@ -10,7 +10,7 @@ public class Topic_01_Check_Environment {
     WebDriver driver;
 
     @Test
-    public void TC_01_Run_On_Firefox1() {
+    public void TC_01_Run_On_Firefox() {
         driver = new FirefoxDriver();
         driver.get("https://www.facebook.com/");
         driver.quit();
