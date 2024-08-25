6866f5999619a8f3570d4bec2c69bb47adbe9185
Giang My Hoa <hoafgm@gmail.com>
2024-08-25 16:18:54 +0700

    add template

---
+++ selenium/webDriver/Topic_00_Template.java
@@ -0,0 +1,42 @@
+package webDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_00_Template {
+
+    // 1- Setup: OS/ Web/ Browser/ Data/ Page...
+    WebDriver driver;
+
+    @BeforeClass
+    public void initialBrowser() {
+        driver = new FirefoxDriver();
+
+        driver.get("https://demo.nopcommerce.com");
+
+    }
+
+    // 2- Action/ Execute
+    @Test
+    public void TC_01_Register () {
+
+
+    }
+    @Test
+    public void TC_02_Login () {
+
+
+    }
+
+    // 3- Clean
+    @AfterClass
+    public void cleanBrowser() {
+        driver.quit();
+    }
+
+}
