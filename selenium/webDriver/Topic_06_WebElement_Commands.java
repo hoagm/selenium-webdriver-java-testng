a450bb48bdb582f9e1d07e79b95e0cd216033a6d
Giang My Hoa <hoafgm@gmail.com>
2025-01-19 20:00:33 +0700

    topic 24

--- selenium/webDriver/Topic_06_WebElement_Commands.java
+++ selenium/webDriver/Topic_06_WebElement_Commands.java
@@ -28,7 +28,44 @@ public class Topic_06_WebElement_Commands {
     // 2- Action/ Execute
     @Test
     public void TC_01_Element() {
-        driver.findElement(By.cssSelector("")).
+        // Tương tác trực tiếp
+        driver.findElement(By.cssSelector(""));
+
+        // Tương tác nhiều lần lên 1 element
+        WebElement element  = (WebElement) driver.findElements(By.cssSelector("input#id"));
+
+        // xoas dữ liệu trong 1 editable element
+        element.clear();
+
+        // Nhập dữ liệu vào editable element
+        element.sendKeys("");
+
+
+        // element cha dùng 1 loại locator- element con dùng 1 loại locator
+        element.findElement(By.cssSelector("div.input-box")).
+                findElement(By.xpath("//input[@id='middlename']"))   ;
+
+        // element con và cha cùng chung 1 locator
+        element.findElement(By.cssSelector("div.input-box input#middlename"));
+
+        // click lên các element clickable
+        element.click();
+
+        // tương  đương vs  submit thông tin lên serve
+        element.submit();
+
+        // kiểm tra element có hiển thị hay ko
+        // verify thông tin data đã action
+        // áp dụng cho tất cả các element
+        element.isDisplayed();
+
+        // kiêm tra 1 cái element đã đc chọn hay chưa
+        //  áp dụng cho checkbox/radio/dropdown
+        element.isSelected();
+
+        // kiểm tra 1 element có cho  phép thao tác lên hay ko
+        element.isEnabled();
+
 
     }
     @Test
