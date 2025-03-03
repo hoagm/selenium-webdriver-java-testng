1774218c1dae9469a6174209e6e236741a0441c6
hoagm <hoafgm@gmail.com>
2025-03-03 13:46:43 +0700

    Update Topic_20_Iframe.java

--- selenium/webDriver/Topic_20_Iframe.java
+++ selenium/webDriver/Topic_20_Iframe.java
@@ -2,7 +2,10 @@ package webDriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,6 +16,7 @@ public class Topic_20_Iframe {
 
     // 1- Setup: OS/ Web/ Browser/ Data/ Page...
     WebDriver driver;
+    Select select;
 
 
     @BeforeClass
@@ -26,7 +30,7 @@ public class Topic_20_Iframe {
 
     // 2- Action/ Execute
     @Test
-    public void TC_01_ () {
+    public void TC_01_Google_Map () {
         // A chứa iframe B
         driver.get("https://embedgooglemap.net/");
 
@@ -62,8 +66,43 @@ public class Topic_20_Iframe {
 
     }
     @Test
-    public void TC_02_ () {
+    public void TC_02_Formsite () throws InterruptedException {
+        driver.get("https://www.formsite.com/templates/education/campus-safety-survey/");
+        Thread.sleep(3000);
 
+        driver.findElement(By.cssSelector("div#imageTemplateContainer img")).click();
+        Thread.sleep(3000);
+
+        driver.switchTo().frame(driver.findElement(By.cssSelector("div#formTemplateContainer iframe")));
+
+        // select dropdown year
+        new Select(driver.findElement(By.cssSelector("//label[contains(text(),'Year')]/following-sibling::select"))).selectByVisibleText("Senior");
+        Assert.assertEquals(select.getFirstSelectedOption().getText(), "Senior");
+
+        // select dropdown residence
+        new Select(driver.findElement(By.cssSelector("//label[contains(text(),'Residence')]/following-sibling::select"))).selectByVisibleText("South Dorm");
+        Assert.assertEquals(select.getFirstSelectedOption().getText(),"South Dorm");
+
+        // select button Gender
+        WebElement gender = driver.findElement(By.cssSelector("td.highlight label"));
+        gender.click();
+        Thread.sleep(3000);
+        Assert.assertEquals(gender.getText(), "Male");
+
+        // click submit
+        driver.findElement(By.cssSelector("input#FSsubmit")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.invalid_message")).getText(),"Please review the form and correct the highlighted items.");
+
+        // click login
+        driver.switchTo().defaultContent();
+
+        driver.findElement(By.cssSelector("nav[class='header header--desktop'] a.menu-item-login")).click();
+        driver.findElement(By.cssSelector("button#login")).click();
+
+        Thread.sleep(3000);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("div#message-error")).getText(), "Username and password are both required.");
 
     }
 
